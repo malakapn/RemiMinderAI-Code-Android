603ae0040395a6f1e880f32cc86d596b13c486f4
Angkon Biswas <angkonbiswas1993@gmail.com>
2026-07-28 19:46:43 -0400

    fix: correct applicationId and namespace to com.remiminderai.app

--- apps/mobile/android/app/build.gradle.kts
+++ apps/mobile/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.remiminder.app.dev"
+    namespace = "com.remiminderai.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.remiminder.app.dev"
+        applicationId = "com.remiminderai.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
